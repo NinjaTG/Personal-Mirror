bd0d42249c8e3ba862451b81342f8dfba34d7d8b
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-05-31 16:51:57 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -106,7 +106,7 @@ def get_readable_message():
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>🎫 Cancel:</b> <code>/cancel2 {download.gid()}</code>"
+                msg += f"\n<b>🎫 Cancel:</b> <code>/cancel3 {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
